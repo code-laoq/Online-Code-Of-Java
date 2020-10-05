6aa46969116729ff9444af40eb8f151e757ad015
秦飞 <69424620+code-laoq@users.noreply.github.com>
2020-10-05 14:03:01 +0800

    Update and rename 1 to task01.java

---
+++ test01/task01.java
@@ -0,0 +1,24 @@
+/*
+【问题描述】
+在控制台输出Hello world!字符串
+【输出形式】
+字符串
+【样例输出】
+Hello world!
+【样例说明】
+按要求输出字符串Hello world!
+【评分标准】
+输出结果正确
+*/
+package test01;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world!");
+
+	}
+
+}
+
